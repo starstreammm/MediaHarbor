5b7cf048fa3d4c15a7e0e8267d4890aa9fdc353a
StarStreammm <xxh_private@outlook.com>
2026-09-06 00:33:23 +0800

    Fix npm build

--- app/hooks/status.tsx
+++ app/hooks/status.tsx
@@ -30,6 +30,8 @@ export function useStatus() {
         retry: 0,
         refetchInterval: 8000,
         refetchIntervalInBackground: true,
+
+        enabled: typeof window !== "undefined",
     });
 
     return query;
